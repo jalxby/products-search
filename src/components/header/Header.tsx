2c1e66b6eff80f005e0bba74a8504cf5107449ca
jalxby <jalxby@gmail.com>
2024-09-02 16:00:16 +0200

    feat: implement styles & typography vars

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <input type={'checkbox'} />
+        <input type={'checkbox'} />
+        <input type={'search'} />
+      </div>
+      <div>
+        <button type={'button'}>Log in</button>
+      </div>
+    </div>
+  )
+}
