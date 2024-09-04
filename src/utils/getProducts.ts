5be163a1c691654334c324921733d11bcaac8674
jalxby <jalxby@gmail.com>
2024-09-04 20:39:41 +0200

    feat: url depends on env

--- src/utils/getProducts.ts
+++ src/utils/getProducts.ts
@@ -1,7 +1,10 @@
 import { FetchProductsParams, FetchProductsResponse } from '@/app/api/products/route'
 
 export async function fetchProducts(params: FetchProductsParams): Promise<FetchProductsResponse> {
-  const response = await fetch('http://localhost:3000/api/products', {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+  const response = await fetch(`${API_URL}/api/products`, {
+    cache: 'no-store',
     headers: {
       'Content-Type': 'application/json',
       'X-Request-Params': JSON.stringify(params),
